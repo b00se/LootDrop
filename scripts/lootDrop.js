942f68821842bb4ecc76450a46407b9cc19033fb
b00se <boos.scro@gmail.com>
2023-04-14 16:03:56 -0600

    init commit

---
+++ scripts/lootDrop.js
@@ -0,0 +1,21 @@
+const moduleName= "lootDrop"
+
+Hooks.on("updateCombat", async (combat, changed, options, userId) => {
+    if (combat.data.scene === game.scenes.active.id) {
+      const combatants = combat.combatants;
+      const allHostileDefeated = combatants.every((combatant) => {
+        const actor = combatant.actor;
+        if (actor && actor.data.type === "npc" && actor.data.data.attributes.hp.value > 0) {
+          return false;
+        }
+        return true;
+      });
+      if (allHostileDefeated) {
+        combatants.every((combatant) => {
+          let token = canvas.token.get(combatant.tokenId)
+          console.log(token)
+        })
+      }
+    }
+  });
+  
