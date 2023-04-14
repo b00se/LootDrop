4dbc18a967880a722d740c20135916ca146e356c
b00se <boos.scro@gmail.com>
2023-04-14 16:27:16 -0600

    log the planet

--- scripts/lootDrop.js
+++ scripts/lootDrop.js
@@ -1,8 +1,10 @@
 const moduleName= "lootDrop"
 
 Hooks.on("updateCombat", async (combat, changed, options, userId) => {
-    if (combat.data.scene === game.scenes.active.id) {
+  log("we get signal")  
+  if (combat.data.scene === game.scenes.active.id) {
       const combatants = combat.combatants;
+      log("mans:", combatants)
       const allHostileDefeated = combatants.every((combatant) => {
         const actor = combatant.actor;
         if (actor && actor.data.type === "npc" && actor.data.data.attributes.hp.value > 0) {
@@ -12,11 +14,18 @@ Hooks.on("updateCombat", async (combat, changed, options, userId) => {
       });
       if (allHostileDefeated) {
         combatants.every((combatant) => {
-          console.log(`FOOBAR combat ended`)
+          log(`combat ended`)
           let token = canvas.token.get(combatant.tokenId)
-          console.log(`FOOBAR $token`)
+          log("token: ", token)
         })
       }
     }
   });
   
+  function log(message, data) {
+    if (data) {
+      console.log(`${moduleName} | ${message}`, data)
+    } else {
+      console.log(`${moduleName} | ${message}`)
+    }
+  }
