d09b3a7874469e125868d56579dfd4ec74a338b6
b00se <boos.scro@gmail.com>
2023-04-15 00:06:36 -0600

    alright it's working now

--- scripts/lootDrop.js
+++ scripts/lootDrop.js
@@ -13,13 +13,12 @@ Hooks.on("deleteCombat", async (combat, options, userId) => {
     combatants.contents.every(async (combatant) => {
       lootLog("got combatant", combatant)
       if (combatant.isNPC && combatant.defeated) {
-        let actor = game.actors.get(combatant.actorId)
         let token = canvas.tokens.get(combatant.tokenId)
         // populate loot
         console.log("Rolling loot")
         await game.itempiles.API.rollItemTable("Merchant: General", {
             timesToRoll: "1d4+1",
-            targetActor: actor,
+            targetActor: token.actor,
             removeExistingActorItems: false
         })
         // make token lootable
@@ -33,35 +32,6 @@ Hooks.on("deleteCombatant", async (combatant, options, userId) => {
   lootLog("about to delete combatant", combatant)
 })
 
-// cc3HIOnfmxXLkxmh
-// cc3HIOnfmxXLkxmh
-
-// Hooks.on("updateCombat", async (combat, changed, options, userId) => {
-//   lootLog("we get signal", combat.scene)
-//   lootLog("changed: ", changed)
-//   lootLog("options: ", options)
-//   lootLog("userId: ", userId)
-//   lootLog("combat.scene", combat.scene)
-//   lootLog("active sceneId", game.scenes.active.id)
-//   if (combat.scene.id === game.scenes.active.id) {
-//       const combatants = combat.combatants;
-//       const allHostileDefeated = combatants.every((combatant) => {
-//         const actor = combatant.actor;
-//         lootLog("mans: ", actor)
-//         if (actor && actor.type === "npc" && actor.system.attributes.hp.value > 0) {
-//           return false;
-//         }
-//         return true;
-//       });
-//       if (allHostileDefeated) {
-//         combatants.every((combatant) => {
-//           lootLog(`combat ended`)
-//           let token = canvas.token.get(combatant.tokenId)
-//           lootLog("token: ", token)
-//         })
-//       }
-//     }
-//   });
   
   function lootLog(message, data) {
     if (data) {
