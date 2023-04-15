25b6cc179a5c83a8a1c663d3d271231454ddf3c3
b00se <boos.scro@gmail.com>
2023-04-14 23:04:24 -0600

    trigger on combatDelete

--- scripts/lootDrop.js
+++ scripts/lootDrop.js
@@ -1,35 +1,60 @@
 const logName= "lootDrop";
+let combatId;
 
 Hooks.on("combatStart", async (combat, updateData) => {
   lootLog("combat", combat);
+  combatId = combat.id
 });
 
-Hooks.on("updateCombat", async (combat, changed, options, userId) => {
-  lootLog("we get signal", combat.scene)
-  lootLog("changed: ", changed)
-  lootLog("options: ", options)
-  lootLog("userId: ", userId)
-  lootLog("combat.scene", combat.scene)
-  lootLog("active sceneId", game.scenes.active.id)
-  if (combat.scene === game.scenes.active.id) {
-      const combatants = combat.combatants;
-      const allHostileDefeated = combatants.every((combatant) => {
-        const actor = combatant.actor;
-        lootLog("mans: ", actor)
-        if (actor && actor.type === "npc" && actor.system.attributes.hp.value > 0) {
-          return false;
-        }
-        return true;
-      });
-      if (allHostileDefeated) {
-        combatants.every((combatant) => {
-          lootLog(`combat ended`)
-          let token = canvas.token.get(combatant.tokenId)
-          lootLog("token: ", token)
+Hooks.on("combatDelete", async (combat, options, userId) => {
+  lootLog("deleting combat", combat)
+  if (combatId !== null && combatId === combat.id) {
+    combatants = combat.combatants
+    combatants.every(async (combatant) => {
+      if (combatant && combatant.isNpc && combatant.resource <= 0) {
+        lootLog("got combatant", combatant)
+        let actor = game.actors.get(combatant.value.actorId)
+        let token = canvas.tokens.get(combatant.value.tokenId)
+        // populate loot
+        console.log("Rolling loot")
+        await game.itempiles.API.rollItemTable("Merchant: General", {
+            timesToRoll: "1d4+1",
+            targetActor: actor,
+            removeExistingActorItems: false
         })
+        // make token lootable
+        await game.itempiles.API.turnTokensIntoItemPiles(token)
       }
-    }
-  });
+    })
+  }
+})
+
+// Hooks.on("updateCombat", async (combat, changed, options, userId) => {
+//   lootLog("we get signal", combat.scene)
+//   lootLog("changed: ", changed)
+//   lootLog("options: ", options)
+//   lootLog("userId: ", userId)
+//   lootLog("combat.scene", combat.scene)
+//   lootLog("active sceneId", game.scenes.active.id)
+//   if (combat.scene.id === game.scenes.active.id) {
+//       const combatants = combat.combatants;
+//       const allHostileDefeated = combatants.every((combatant) => {
+//         const actor = combatant.actor;
+//         lootLog("mans: ", actor)
+//         if (actor && actor.type === "npc" && actor.system.attributes.hp.value > 0) {
+//           return false;
+//         }
+//         return true;
+//       });
+//       if (allHostileDefeated) {
+//         combatants.every((combatant) => {
+//           lootLog(`combat ended`)
+//           let token = canvas.token.get(combatant.tokenId)
+//           lootLog("token: ", token)
+//         })
+//       }
+//     }
+//   });
   
   function lootLog(message, data) {
     if (data) {
